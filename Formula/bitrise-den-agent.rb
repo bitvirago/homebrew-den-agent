eec45466ab16fcff8eca28f5cc5750b9ebcca1ef
Dmytro Hridin <dmytro.hridin@bitrise.io>
2023-06-14 00:09:24 +0300

    update sha

--- Formula/bitrise-den-agent.rb
+++ Formula/bitrise-den-agent.rb
@@ -6,17 +6,17 @@ class BitriseDenAgent < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/bitrise-io/bitrise-den-agent/releases/download/v2.1.5-alpha/bitrise-den-agent-darwin-arm64.zip"
-      sha256 "efa19e324e5902b7a2b9c489823ed6bdddbe9168c1a2815f8cf1849c1b4a0de0"
+      sha256 "f54833680b26ccf4532f30b0ab3b7e322dfb1cb37cb29dfd21545919650ef9f3"
     end
     if Hardware::CPU.intel?
       url "https://github.com/bitrise-io/bitrise-den-agent/releases/download/v2.1.5-alpha/bitrise-den-agent-darwin-amd64.zip"
-      sha256 "410ac10497651b2555d7e09941409f3b8f8469abe957e898f9ace42141d54540"
+      sha256 "ae594544497c8d681e206fcecacddedd0a352b7a03aaf12b77a05e202254eb9d"
     end
   end
 
   on_linux do
     url "https://github.com/bitrise-io/bitrise-den-agent/releases/download/v2.1.5-alpha/bitrise-den-agent-linux-amd64.zip"
-    sha256 "72fe8137e9f6b4b1ce45a74053af2f6daff0abab1d06e694b943027e473fcdaa"
+    sha256 "04a11108b1e3cbe156f1196efc3fe7a6f62e3d466b3c316aed5f350b716b6c58"
   end
 
   def install
