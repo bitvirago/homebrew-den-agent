7c72e9443a852417ad378a4f3390adf9af715a23
Dmytro Hridin <dmytro.hridin@bitrise.io>
2023-06-13 22:06:17 +0300

    add check for macos arm

--- Formula/bitrise-den-agent.rb
+++ Formula/bitrise-den-agent.rb
@@ -1,10 +1,14 @@
 class BitriseDenAgent < Formula
   desc "CLI for Bitrise DEN agent"
   homepage "https://github.com/bitrise-io/bitrise-den-agent"
-  url "https://github.com/bitrise-io/bitrise-den-agent/releases/download/v.0.0.1-alpha/bitrise-den-agent.zip"
-  sha256 "f54833680b26ccf4532f30b0ab3b7e322dfb1cb37cb29dfd21545919650ef9f3"
   license "MIT"
 
+  on_macos do
+    if Hardware::CPU.arm?
+      url "https://github.com/bitrise-io/bitrise-den-agent/releases/download/v.0.0.1-alpha/bitrise-den-agent.zip"
+      sha256 "f54833680b26ccf4532f30b0ab3b7e322dfb1cb37cb29dfd21545919650ef9f3"
+    end
+  end
   def install
     bin.install "bitrise-den-agent"
   end
