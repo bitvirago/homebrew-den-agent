3c2a7eb76abc85e7db1dd7aef7c8e7e6b4ef84e3
Dmytro Hridin <dmytro.hridin@bitrise.io>
2023-06-13 16:16:22 +0300

    update sha

--- Formula/bitrise-den-agent.rb
+++ Formula/bitrise-den-agent.rb
@@ -2,7 +2,7 @@ class BitriseDenAgent < Formula
   desc "CLI for Bitrise DEN agent"
   homepage "https://github.com/bitrise-io/bitrise-den-agent"
   url "https://github.com/bitrise-io/bitrise-den-agent/releases/download/v.0.0.1-alpha/bitrise-den-agent.zip"
-  sha256 "a7b33a2d62b62bb0d61e0dba2eaa4b7b6b6a67b38b0d4fe21210caa87998a491"
+  sha256 "f54833680b26ccf4532f30b0ab3b7e322dfb1cb37cb29dfd21545919650ef9f3"
   license "MIT"
 
   def install
