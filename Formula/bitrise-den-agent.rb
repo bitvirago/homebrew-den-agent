f9c46d9ba3e4091d3c1e8c829d0f69685eb63746
Dmytro Hridin <dmytro.hridin@bitrise.io>
2023-06-13 16:14:42 +0300

    change tar gz to zip

--- Formula/bitrise-den-agent.rb
+++ Formula/bitrise-den-agent.rb
@@ -1,7 +1,7 @@
 class BitriseDenAgent < Formula
   desc "CLI for Bitrise DEN agent"
   homepage "https://github.com/bitrise-io/bitrise-den-agent"
-  url "https://github.com/bitrise-io/bitrise-den-agent/releases/download/v.0.0.1-alpha/bitrise-den-agent.tar.gz"
+  url "https://github.com/bitrise-io/bitrise-den-agent/releases/download/v.0.0.1-alpha/bitrise-den-agent.zip"
   sha256 "a7b33a2d62b62bb0d61e0dba2eaa4b7b6b6a67b38b0d4fe21210caa87998a491"
   license "MIT"
 
